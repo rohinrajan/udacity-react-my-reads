8f653f305cff10f8c78bc9333425634a5f5f5b6e
Rohin Rajan <rajan.rohin@gmail.com>
2017-08-23 00:34:22 -0700

    added the three categories

--- src/ListBooks.js
+++ src/ListBooks.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import './App.css'
+
 class ListBooks extends Component {
  state = {
    display : 'false'
@@ -8,33 +9,109 @@ class ListBooks extends Component {
   render(){
     const {books, displayList} = this.props
     console.log(books)
-
+    const currentreading = books.filter((book) => (book.shelf === "currentlyReading"))
+    const wantToRead = books.filter((book) => (book.shelf === "wantToRead"))
+    const read = books.filter((book) => (book.shelf === "read"))
     return (
-      <ol>
-        { displayList && books.map((book)=> (
-          <li key={book.id}>
-            <div className="book">
-              <div className="book-top">
-                <div className="book-cover"
-                  style={{ width: 128, height: 192,
-                    backgroundImage: `url(${book.imageLinks.thumbnail})` }}>
-                </div>
-                <div className="book-shelf-changer">
-                  <select>
-                    <option value="none" disabled>Move to...</option>
-                    <option value="currentlyReading">Currently Reading</option>
-                    <option value="wantToRead">Want to Read</option>
-                    <option value="read">Read</option>
-                    <option value="none">None</option>
-                  </select>
-                </div>
+      <div className="list-books">
+        <div className="list-books-title">
+            <h1>Rohin's Version of MyReads</h1>
+        </div>
+        <div className="list-books-content">
+          <div>
+            <div className="bookshelf">
+              <h2 className="bookshelf-title">Currently Reading</h2>
+              <div className="bookshelf-books">
+                <ol className="books-grid">
+                  { displayList && currentreading.map((book)=> (
+                    <li key={book.id}>
+                      <div className="book">
+                        <div className="book-top">
+                          <div className="book-cover"
+                            style={{ width: 128, height: 192,
+                              backgroundImage: `url(${book.imageLinks.thumbnail})` }}>
+                          </div>
+                          <div className="book-shelf-changer">
+                            <select>
+                              <option value="none" disabled>Move to...</option>
+                              <option value="currentlyReading">Currently Reading</option>
+                              <option value="wantToRead">Want to Read</option>
+                              <option value="read">Read</option>
+                              <option value="none">None</option>
+                            </select>
+                          </div>
+                        </div>
+                        <div className="book-title">{book.title}</div>
+                        <div className="book-authors">{JSON.stringify(book.authors)}</div>
+                      </div>
+                    </li>
+                  ))}
+                </ol>
               </div>
-              <div className="book-title">{book.title}</div>
-              <div className="book-authors">{JSON.stringify(book.authors)}</div>
             </div>
-          </li>
-        ))}
-      </ol>
+            <div className="bookshelf">
+              <h2 className="bookshelf-title">Want to Read</h2>
+              <div className="bookshelf-books">
+                <ol className="books-grid">
+                  { displayList && wantToRead.map((book)=> (
+                    <li key={book.id}>
+                      <div className="book">
+                        <div className="book-top">
+                          <div className="book-cover"
+                            style={{ width: 128, height: 192,
+                              backgroundImage: `url(${book.imageLinks.thumbnail})` }}>
+                          </div>
+                          <div className="book-shelf-changer">
+                            <select>
+                              <option value="none" disabled>Move to...</option>
+                              <option value="currentlyReading">Currently Reading</option>
+                              <option value="wantToRead">Want to Read</option>
+                              <option value="read">Read</option>
+                              <option value="none">None</option>
+                            </select>
+                          </div>
+                        </div>
+                        <div className="book-title">{book.title}</div>
+                        <div className="book-authors">{JSON.stringify(book.authors)}</div>
+                      </div>
+                    </li>
+                  ))}
+                </ol>
+              </div>
+            </div>
+            <div className="bookshelf">
+              <h2 className="bookshelf-title">Read</h2>
+              <div className="bookshelf-books">
+                <ol className="books-grid">
+                  { displayList && read.map((book)=> (
+                    <li key={book.id}>
+                      <div className="book">
+                        <div className="book-top">
+                          <div className="book-cover"
+                            style={{ width: 128, height: 192,
+                              backgroundImage: `url(${book.imageLinks.thumbnail})` }}>
+                          </div>
+                          <div className="book-shelf-changer">
+                            <select>
+                              <option value="none" disabled>Move to...</option>
+                              <option value="currentlyReading">Currently Reading</option>
+                              <option value="wantToRead">Want to Read</option>
+                              <option value="read">Read</option>
+                              <option value="none">None</option>
+                            </select>
+                          </div>
+                        </div>
+                        <div className="book-title">{book.title}</div>
+                        <div className="book-authors">{JSON.stringify(book.authors)}</div>
+                      </div>
+                    </li>
+                  ))}
+                </ol>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     )
   }
 }
