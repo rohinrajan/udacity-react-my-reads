5ffd6c5a767091b74e9c28ff0cb4ee2b9ef7545c
Rohin Rajan <rajan.rohin@gmail.com>
2017-09-17 08:43:47 -0700

    minor correction in the project

--- src/App_new.js
+++ src/App_new.js
@@ -37,13 +37,13 @@ class BooksApp extends React.Component {
 
   render(){
     return(
-      // <div>
-      //   <ListBooks books={this.state.books} displayList={this.state.displayList}
-      //     updateBookShelf={this.onUpdateBooks}/>
-      // </div>
       <div>
-        <SearchBook onSearchResult={this.searchBook} currentBookList={this.state.books}/>
+        <ListBooks books={this.state.books} displayList={this.state.displayList}
+          updateBookShelf={this.onUpdateBooks}/>
       </div>
+      // <div>
+      //   <SearchBook onSearchResult={this.searchBook} currentBookList={this.state.books} appendCurrentBookToList={this.onUpdateBooks}/>
+      // </div>
     )
   }
 }
