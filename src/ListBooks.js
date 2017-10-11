109aed7e47008ec4816c1c67b5235e7f0b15b07d
Rohin Rajan <rajan.rohin@gmail.com>
2017-10-11 00:19:52 -0700

    made listbook statless functional component

--- src/ListBooks.js
+++ src/ListBooks.js
@@ -1,42 +1,32 @@
-import React, { Component } from 'react'
+import React from 'react'
 import './App.css'
 import { Link } from 'react-router-dom'
-import { PropTypes } from 'prop-types'
 import BookShelf from './BookShelf'
 
-class ListBooks extends Component {
-
- static propTypes = {
-   books : PropTypes.array.isRequired,
-   displayList : PropTypes.bool.isRequired,
-   updateBookShelf : PropTypes.func.isRequired
- }
-
-  render(){
-    const {books, displayList, updateBookShelf} = this.props
-    // console.log(books)
-    const currentreading = books.filter((book) => (book.shelf === "currentlyReading"))
-    const wantToRead = books.filter((book) => (book.shelf === "wantToRead"))
-    const read = books.filter((book) => (book.shelf === "read"))
-    return (
-      <div className="list-books">
-        <div className="list-books-title">
-            <h1>My Reads</h1>
-        </div>
-        <div className="list-books-content">
-          <div>
-            { displayList && (<BookShelf bookList={currentreading} book_title="currently Reading" updateBookShelf={updateBookShelf}/>)}
-            { displayList && (<BookShelf bookList={wantToRead} book_title="Want to Read" updateBookShelf={updateBookShelf}/>)}
-            { displayList && (<BookShelf bookList={read} book_title="Read" updateBookShelf={updateBookShelf}/>)}
-          </div>
-        </div>
-        <div className="open-search">
-          <Link to="/search"
-            >Add a book</Link>
+function ListBooks(props){
+  const {books, displayList, updateBookShelf} = props;
+  // console.log(books)
+  const currentreading = books.filter((book) => (book.shelf === "currentlyReading"));
+  const wantToRead = books.filter((book) => (book.shelf === "wantToRead"));
+  const read = books.filter((book) => (book.shelf === "read"));
+  return (
+    <div className="list-books">
+      <div className="list-books-title">
+          <h1>My Reads</h1>
+      </div>
+      <div className="list-books-content">
+        <div>
+          { displayList && (<BookShelf bookList={currentreading} book_title="currently Reading" updateBookShelf={updateBookShelf}/>)}
+          { displayList && (<BookShelf bookList={wantToRead} book_title="Want to Read" updateBookShelf={updateBookShelf}/>)}
+          { displayList && (<BookShelf bookList={read} book_title="Read" updateBookShelf={updateBookShelf}/>)}
         </div>
       </div>
-    )
-  }
+      <div className="open-search">
+        <Link to="/search"
+          >Add a book</Link>
+      </div>
+    </div>
+  )
 }
 
 export default ListBooks
